7c72443495c03d0ec6ba75c7a1ac53727e8f4059
alokd <alokd@KNOXY>
2023-10-21 12:02:17 +0530

    Liskov part1

---
+++ LiskovDemo/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovDemo
+{
+        public abstract class Employee
+        {
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            
+
+            public Employee() { }
+            
+            public Employee(int id, string name)
+            {
+                this.Id = id;
+                this.Name = name;
+
+            }
+           
+            public abstract decimal CalculateBonus(decimal salary);
+           
+            public override string ToString()
+            {
+                return string.Format("ID : {0} Name :{1}", this.Id, this.Name);
+            }
+        }
+        public class PermanentEmployee : Employee   
+        {
+            public PermanentEmployee()
+            { }
+            public PermanentEmployee(int id, string name) : base(id, name)
+            { }
+            public override decimal CalculateBonus(decimal salary)
+            {
+                return salary * .1M;
+            }
+        }
+        public class TemporaryEmployee : Employee
+        {
+            public TemporaryEmployee() { }
+            public TemporaryEmployee(int id, string name) : base(id, name) { }
+            public override decimal CalculateBonus(decimal salary)
+            {
+                return salary * 0.5M;
+            }
+
+        }
+    /// <summary>
+    /// 2nd so here we create one new derived class ContractEmployee in which we 
+    /// </summary>
+    public class ContractEmployee: Employee  
+    {
+        public ContractEmployee() { }
+        public ContractEmployee(int id, string name) : base(id,name) { }
+
+        public override decimal CalculateBonus(decimal salary)
+        {
+            throw new NotImplementedException();  //3rd ContractEmployee is not eligble for any bonus hence we throw exception here in the implementation of CalculateBonus
+        }
+
+    }
+}
+
---
+++ LiskovDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovDemo
+{
+
+    /// <summary>
+    /// 1st This Example also satisfied the Liskov substitution principle partialy because it is not following one of the implementation guideline of liskov that is no new exception can be thrown by the sub type 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empJhon = new PermanentEmployee(1, "John");
+            Employee empJason = new TemporaryEmployee(2, "Jason");
+
+            Employee empMike = new ContractEmployee(3, "Mike"); //4th now we add empMike as Contract employee
+
+            Console.WriteLine(string.Format("Emploee {0} Bonus :{1}",
+                empJhon.ToString(),
+                empJhon.CalculateBonus(100000).ToString()));
+            Console.WriteLine(string.Format("Emploee {0} Bonus :{1}",
+               empJason.ToString(),
+               empJason.CalculateBonus(150000).ToString()));
+            Console.WriteLine(string.Format("Emploee {0} Bonus :{1}",
+              empMike.ToString(),
+              empMike.CalculateBonus(150000).ToString()));
+            Console.ReadLine();
+        }
+    }
+}
+//note : if we run program after step 4th step it will thow exception and unsatisfied implementation of liskov
+// 5th create a new app that will satisfy the guidline for the implementation of liskov principle
