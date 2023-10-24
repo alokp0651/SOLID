e2b0d3d5a94ee6ec470f2704180e5766481565f8
alokd <alokd@KNOXY>
2023-10-24 17:09:21 +0530

    Dependency Inversion Principle (DIP)

---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{/// <summary>
+ /// The Dependency Inversion Principle (DIP) is one of the SOLID principles of object-oriented design and encourages decoupling between high-level and low-level modules in your code. In C#, this is often achieved through the use of interfaces and dependency injection. Here's a simple example to illustrate the Dependency Inversion Principle:
+ /// Suppose you have a high-level class called Car that depends on a low-level class called Engine.Without following the Dependency Inversion Principle, your code might look like this:
+/// </summary>
+    public class Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Engine started");
+        }
+    }
+
+    public class Car
+    {
+        private Engine engine;
+
+        public Car()
+        {
+            engine = new Engine();
+        }
+
+        public void StartCar()
+        {
+            engine.Start();
+            Console.WriteLine("Car started");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.StartCar();
+        }
+    }
+}
---
+++ DependencyInversionSolotuion/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionSolotuion
+{
+    /// <summary>
+    /// In the above code, the Car class directly depends on the Engine class, violating the Dependency Inversion Principle. To follow DIP, you should introduce an interface to abstract the low-level dependency (Engine) and use dependency injection to provide the concrete implementation. Here's the modified code:
+    /// </summary>
+    public interface IEngine
+    {
+        void Start();
+    }
+
+    public class Engine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Engine started");
+        }
+    }
+
+    public class Car
+    {
+        private IEngine engine;
+
+        public Car(IEngine engine)
+        {
+            this.engine = engine;
+        }
+
+        public void StartCar()
+        {
+            engine.Start();
+            Console.WriteLine("Car started");
+        }
+    }
+    /// <summary>
+    /// The example  provided illustrates the Dependency Inversion Principle (DIP) by using interfaces and dependency injection in C#. It's a simplified example to demonstrate the concept, but it is indeed a correct and valid way to follow DIP. 
+
+      //In this example:
+
+    //1. An interface `IEngine` is introduced to define a contract that any engine implementation should adhere to.
+    //2. The `Engine` class implements the `IEngine` interface, ensuring that it provides the required functionality.
+   //3. The `Car` class takes an `IEngine` object as a constructor parameter, allowing for dependency injection.This means that you can supply any class that implements the `IEngine` interface when creating a `Car` object, making it more flexible and following the DIP.
+
+      //This example effectively decouples the `Car` class from the concrete implementation of the engine, promoting the principles of dependency inversion.If you have any specific concerns or questions about this example, please feel free to ask, and I'll be happy to provide further clarification.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEngine carEngine = new Engine();
+            Car car = new Car(carEngine);
+            car.StartCar();
+        }
+    }
+}
